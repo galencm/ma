b72ff2058987575e465f4b33cd5bb72f2a7333cb
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-08 01:33:14 +0000

    Problem: output defaults to codegen directory
    Solution: add argument for output path to to avoid
    having to move generated files from codegen directory
    to machine directory.

--- codegen/codegen.sh
+++ codegen/codegen.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-usage="Usage:  $0 [xml] [name]
-        xml     path to xml file
-        name    (default: 'machine') used to create output directory
+usage="Usage:  $0 [xml] [name] [output path] [primitive directory]
+        xml               path to xml file
+        name              (default: 'machine') used to create output directory
+        output path       directory to store generated files
+        primitive source  (default: ./PRIMITIVES) directory containing gsl files
 
 Use an xml file to    1) generate yaml file for machine and 
                       2) generate isonomic permutations(cli,gui,iot,etc...)
 
 Generated files are stored in created directory structure.
 For example:
-    ./codegen.sh foo.xml
+    ./codegen.sh ~/foo.xml machine_foo ~/machine_foo
 produces the output:
-                      ./machine/
-                      ./machine/machine.yaml
-                      ./machine/generated/<files>
+                      ~/machine_foo/
+                      ~/machine_foo/machine.yaml
+                      ~/machine_foo/generated/<files>
 "
 # show usage if run without arguments
 : ${1?"$usage"}
 #if run without second argument use 'machine'
 #as default
+machine_xml=$1
 name=${2:-machine}
-primitive_source=${3:-./PRIMITIVES}
+machine_path=${3:-./machine}
+primitive_source=${4:-./PRIMITIVES}
 
 while getopts ':hs:' option; do
   case "$option" in
@@ -30,12 +34,10 @@ while getopts ':hs:' option; do
 done
 shift $((OPTIND - 1))
 
-#$1 xml file
-#$2  output name
-gsl -a -script:machine_yaml.gsl $1 $name 
-#$1 xml file
-#$2 output directory in form of ./foo/ with trailing /
-#$3 location of gsl files to use as templates
-gsl -a -script:permutate.gsl $1 ./$name/generated/ $primitive_source
-#check generated yaml
-python3 check_yaml.py ./$name/$name.yaml
+
+gsl -a -script:machine_yaml.gsl $machine_xml $name $machine_path
+
+# machine_path trailing slash is important!
+gsl -a -script:permutate.gsl $machine_xml $machine_path/generated/ $primitive_source
+# check generated yaml
+#python3 check_yaml.py $machine_path/$name.yaml
