9b2d754b90b9f1a5921f482b31e28f47cae4c715
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-09 09:08:17 +0000

    Problem: program feedback is inscrutable
    Solution: more descriptive logging

--- machinic/jobs.py
+++ machinic/jobs.py
@@ -43,6 +43,7 @@ def list_jobs(nomad_location=None):
 def list_services():
     c = consul.Consul()
     services = {k:v for (k,v) in c.agent.services().items() if k.startswith("_nomad")}
+    logger.info("retrieving service details from consul")
     print("{}\t\t{}\t\t{}".format("Service","Ip","Port"))
     for k in services.keys():
         print("{}\t{}\t{}".format(services[k]['Service'].ljust(15),services[k]['Address'],services[k]['Port']))
@@ -54,8 +55,7 @@ def get_job(name,path=".jobs",file=None,nomad_location=None):
     nomad,nomad_ip,nomad_port = nomad_details(nomad_location)
     gs = 'http://{}:{}/v1/job/{}'.format(nomad_ip,nomad_port,name)
     r = requests.get(gs)
-    print(r.json())
-    print(r.text)
+    logger.info(r.text)
 
     if file is not False:
         if file is None:
@@ -111,15 +111,15 @@ def run_job(name,command,args,path=".jobs",tags=None,verbose=False,nomad_locatio
     args = list(filter(None, args)) 
 
     if not os.path.exists(os.path.abspath(path)):
+        logger.info("creating: {}".format(os.path.abspath(path)))
         os.makedirs(path)
 
-    logger.info("running {}".format(name))
+    logger.info("job path: {}".format(os.path.abspath(path)))
+
+    logger.info("preparing to run job: {}".format(name))
     nomad,nomad_ip,nomad_port = nomad_details(nomad_location)
-    #"{} run -output test.hcl  > /tmp/test.json".format(nomad)
-    #"curl -X PUT {}:{}/v1/job/rpcz -d @/tmp/test.json".format(nomad_ip,nomad_port)
-              #{% for arg in args -%}
-              #"{{arg}}"{{ "," if not loop.last }}
-              #{%- endfor %}
+    logger.info("nomad address: http://{}:{}".format(nomad_ip,nomad_port))
+
     config = {}
     config['name'] = name
     if tags:
@@ -186,59 +186,57 @@ def run_job(name,command,args,path=".jobs",tags=None,verbose=False,nomad_locatio
     }
     ''')
 
+    logger.info("rendering hcl from args dict: {}".format(config))
     job_hcl = jinja2.Environment().from_string(job_template).render(config)
     
     #do not overwrite if using external kwarg
     #in case of reload or fg
     if not external_file:
+        logger.info("writing job .hcl file: {}".format(os.path.join(path,'{}.hcl'.format(name))))
         with open(os.path.join(path,'{}.hcl'.format(name)),'w+') as f:
             f.write(job_hcl)
 
+        logger.info("converting .hcl to .json for submission")
         job_json = subprocess.check_output('{} run -output {}.hcl'.format(nomad,os.path.join(path,name)).split()) 
         job_json=job_json.decode()
 
+        logger.info("loading json")
         j = json.loads(job_json)
 
     if external_file:
         #external_file = os.path.join(path,external_file)
-        logger.info("loading {}".format(external_file))
-        print("loading {}".format(external_file))
+        logger.info("loading job file: {}".format(external_file))
 
         if external_file.endswith('.hcl'):
+            logger.info("converting .hcl to .json for submission")
             job_json = subprocess.check_output('{} run -output {}'.format(nomad,external_file).split()) 
             job_json=job_json.decode()
+            logger.info("loading json")
             j = json.loads(job_json)
 
         elif external_file.endswith('.json'):
+            logger.info("loading json from file {}".format(external_file))
             with open(external_file) as json_data:
                 j = json.load(json_data)
 
-        print("-------------------------------")
-        print(external_file)
-        print(j)
-        print("-------------------------------")
-
     if verbose:
         print(j)
 
-    req = 'http://{}:{}/v1/job/{}'.format(nomad_ip,nomad_port,name)
-    if verbose:
-        print(req)
-    r = requests.put(req, json=j)
-    print(r.status_code)
-    print(j)
+    job_submit_endpoint = 'http://{}:{}/v1/job/{}'.format(nomad_ip,nomad_port,name)
+    logger.info("submitting(PUT) job json to: {}".format(job_submit_endpoint))
+    r = requests.put(job_submit_endpoint, json=j)
+    logger.info("{}".format(r))
+    logger.info(j)
 
-    if r.status_code == 400:
-        foo = subprocess.check_output('{} run {}'.format(nomad,external_file).split()) 
-        print(foo)
-    print(r)
+    # if r.status_code == 400:
+    #     foo = subprocess.check_output('{} run {}'.format(nomad,external_file).split()) 
+    #     print(foo)
 
-    fname = '{}.json'.format(name)
+    json_file = '{}.json'.format(name)
 
-    with open(os.path.join(path,fname),'w+') as f:
+    logger.info("writing job .json file: {}".format(os.path.join(path,json_file)))
+    with open(os.path.join(path,json_file),'w+') as f:
         f.write(json.dumps(j, indent=4))
-
-    #r = requests.put('{}:{}/v1/job/{}'.format(nomad_ip,nomad_port,name), json=json.loads(job_json))
 
 def foo(args):
     if 'status' in args or 'status-raw' in args:
@@ -248,7 +246,7 @@ def foo(args):
 
 def run_existing(argv):
     #parser.add_argument("--command",required='run' in argv , help="full path of command to call")
-    print("!!!!!!!",argv)
+    logger.info("args: {}".format(argv))
     job_name = ""
     for i,arg in enumerate(argv):
         if arg == "--name":
@@ -257,18 +255,14 @@ def run_existing(argv):
             except:
                 return True
     job = os.path.join(os.path.expanduser('~/.local/jobs'),job_name)
-    print(job)
     #sys.exit()
     if 'run' in argv and '--existing-file' in argv:
         return False
     elif 'run' in argv and os.path.isfile(job):
-        print("?")
         return True
     elif 'run' in argv:
-        print("?z")
         return True
     else:
-        print("???????????")
         return False
 
 def main(argv):
@@ -303,7 +297,7 @@ def main(argv):
     parser.add_argument("-v","--verbose",action="store_true", help="verbose")
     parser.add_argument("--existing-file", help="",default=False)
     parser.add_argument("-c","--checks",help="checks ie gphoto2")
-    parser.add_argument("--log-level", choices=['debug','info','warn','error'],default="error",help="checks ie gphoto2")
+    parser.add_argument("--log-level", choices=['debug','info','warn','error'],default="info",help="checks ie gphoto2")
 
     jobs_path = os.path.expanduser('~/.local/jobs')
 
@@ -313,6 +307,7 @@ def main(argv):
 
     #TODO tutorial string on error with no arguments
     args = parser.parse_args()  
+
     if args.nomad_location is None:
         args.nomad_location = "nomad"
 
