f45a1257d13fc89148a34e548f76dafd4acdef4e
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-11 07:04:45 +0000

    Problem: no python shebang in file
    Solution: add shebang

--- mchgen/helloworlds.py
+++ mchgen/helloworlds.py
@@ -1,8 +1,10 @@
-from logzero import logger
+#!/usr/bin/python3
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # Copyright (c) the Contributors as noted in the AUTHORS file.
+from logzero import logger
+
 def hello():
     logger.info("hello worlds!")
     return "hello worlds!"
